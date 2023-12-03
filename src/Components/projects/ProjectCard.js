82c1daa63f3e2dc71960a11b725ee963ee99ae44
MahmoudSamir2003 <mahmoudsamir212089@gmail.com>
2023-12-03 15:01:52 +0200

    mahmoud

--- src/Components/projects/ProjectCard.js
+++ src/Components/projects/ProjectCard.js
@@ -10,7 +10,7 @@ const ProjectCard = ({ imageUrl, projectInfo, externalLink }) => {
 
 <div className={`card-container container col-12 col-sm-4 col-md-4 ${darkMode ? 'dark-mode-class' : ''}`} onClick={handleCardClick}>
     <div className="card-inner row ">
-        <div className="card-front  m-3">
+        <div className="card-front  mt-4">
           <img src={imageUrl} alt="Project" className="img-fluid" />
         </div>
         <div className="card-back d-flex align-items-center justify-content-center">
